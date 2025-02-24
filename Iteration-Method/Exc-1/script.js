123e1052e652cb97230a707ea434bcaa69454570
JoaoNascimento1802 <joao.emanuel.pn@gmail.com>
2025-02-24 11:05:29 -0300

    :tada: Commit inicial

---
+++ Iteration-Method/Exc-1/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("button1")
+
+button.addEventListener("click" , function(event){
+    alert("Botao Molestado" , );
+    console.log("Id" , event.target.id);
+});
---
+++ Iteration-Method/Exc-2/script.js
@@ -0,0 +1,9 @@
+const box = document.getElementById("box");
+
+box.addEventListener("mouseover" , function(){
+    box.style.backgroundColor = "#993399";
+});
+
+box.addEventListener("mouseout" , function(){
+    box.style.backgroundColor = "#ffffff";  
+});
